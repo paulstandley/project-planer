4932bc330c02291dfcc7ed1614aba2b9521d61b2
Paul Standley <paulstandley1972@gmail.com>
2019-02-19 19:34:27 +0000

    add sign in sign out components

---
+++ src/components/auth/SignIn.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class SignIn extends Component {
+  state = { 
+    email: '',
+    password: ''
+   }
+
+  handelChange = (evt) => {
+    this.setState({
+      [evt.target.id]: evt.target.value
+    });
+  }
+
+  handelSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(this.state);
+  }
+
+  render() { 
+    return ( 
+      <div className="container">
+        <form onSubmit={this.handelSubmit} className="white">
+          <h5 className="grey-text text-darken-3">Sign In</h5>
+          <div className="input-field">
+            <label htmlFor="email">Email</label>
+            <input  type="email" id="email" onChange={this.handelChange}/>
+          </div>
+          <div className="input-field">
+            <label htmlFor="password">Password</label>
+            <input  type="password" id="password" onChange={this.handelChange}/>
+          </div>
+          <div className="input-field">
+            <button className="btn pink lighten-1 z-depth-0">Login></button>
+          </div>
+        </form>
+      </div>
+     );
+  }
+}
+ 
+export default SignIn;
---
+++ src/components/auth/SignUp.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+class SignUp extends Component {
+  state = { 
+    email: '',
+    password: '',
+    firstName: '',
+    lastName: ''
+   }
+
+  handelChange = (evt) => {
+    this.setState({
+      [evt.target.id]: evt.target.value
+    });
+  }
+
+  handelSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(this.state);
+  }
+
+  render() { 
+    return ( 
+      <div className="container">
+        <form onSubmit={this.handelSubmit} className="white">
+          <h5 className="grey-text text-darken-3">Sign Up</h5>
+          <div className="input-field">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" onChange={this.handelChange}/>
+          </div>
+          <div className="input-field">
+            <label htmlFor="password">Password</label>
+            <input  type="password" id="password" onChange={this.handelChange}/>
+          </div>
+          <div className="input-field">
+            <label htmlFor="firstName">First Name</label>
+            <input  type="text" id="firstName" onChange={this.handelChange}/>
+          </div>
+          <div className="input-field">
+            <label htmlFor="lastName">Last Name</label>
+            <input  type="text" id="lastName" onChange={this.handelChange}/>
+          </div>
+          <div className="input-field">
+            <button className="btn pink lighten-1 z-depth-0">Sign Up></button>
+          </div>
+        </form>
+      </div>
+     );
+  }
+}
+ 
+export default SignUp;
