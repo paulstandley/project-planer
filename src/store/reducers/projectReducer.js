973b1e8555f5f8505bea23d93cfa0cdd7b08fa1d
Paul Standley <paulstandley1972@gmail.com>
2019-02-20 21:37:44 +0000

    setup redux pass down and display

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,7 +1,13 @@
-const initState = {};
+const initState = {
+    projects: [
+        { id: "1", title: "help find a peach", content: "blar blah bla" },
+        { id: "2", title: "help cook the peach", content: "blar blah bla" },
+        { id: "3", title: "help to eat peach", content: "blar blah bla" }
+    ]
+};
 
 const projectReducer = (state = initState, action) => {
-  return state;
+    return state;
 }
 
 export default projectReducer;
