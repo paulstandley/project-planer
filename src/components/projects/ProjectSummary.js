299ed57de03507426589fda5fe7efb46e43bf419
Paul Standley <paulstandley1972@gmail.com>
2019-02-26 20:57:01 +0000

    display project?

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -3,11 +3,11 @@ import React from 'react'
 const ProjectSummary = ({ project })  => {
   return (
     <div>
-            <div className="card z-depth-0 project-summary">
+      <div className="card z-depth-0 project-summary">
         <div className="card-content gray-text text-darken-3">
-        <span className="card-title">{project.title}</span>
-          <p>Posted by Lou</p>
-          <p className="gray-text">17 Feb 21:23</p>
+          <span className="card-title">{project.title}</span>
+            <p>{project.content}</p>
+            <p className="gray-text">17 Feb 21:23</p>
         </div>
       </div>
     </div>
