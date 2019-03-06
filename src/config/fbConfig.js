cdd19ee2e214ee57b5f54a5651a52ed0f15bda3b
Paul Standley <paulstandley1972@gmail.com>
2019-03-06 20:37:37 +0000

    log out of firebase

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,12 +4,12 @@ import 'firebase/auth';
 
 // Initialize Firebase
 var config = {
-  apiKey: "API_KEY",
-  authDomain: "project-planer-8d7d7.firebaseapp.com",
-  databaseURL: "https://project-planer-8d7d7.firebaseio.com",
-  projectId: "project-planer-8d7d7",
-  storageBucket: "project-planer-8d7d7.appspot.com",
-  messagingSenderId: "897259823180"
+    apiKey: "API_KEY",
+    authDomain: "project-planer-8d7d7.firebaseapp.com",
+    databaseURL: "https://project-planer-8d7d7.firebaseio.com",
+    projectId: "project-planer-8d7d7",
+    storageBucket: "project-planer-8d7d7.appspot.com",
+    messagingSenderId: "897259823180"
 };
 
 firebase.initializeApp(config);
--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,23 +1,26 @@
 const initState = {
-  authError: null
+    authError: null
 };
 
 const authReducer = (state = initState, action) => {
-  switch(action.type) {
-    case 'LOGIN_ERROR':
-    console.error('login error') 
-      return {
-        ...state,
-        authError: 'Login failed'
-      }
-      case 'LOGIN_SUCCESS':
-      console.log('login success') 
-        return {
-          ...state,
-          authError: null
-        }
+    switch (action.type) {
+        case 'LOGIN_ERROR':
+            console.error('login error')
+            return {
+                ...state,
+                authError: 'Login failed'
+            }
+        case 'LOGIN_SUCCESS':
+            console.log('login success')
+            return {
+                ...state,
+                authError: null
+            }
+        case 'SIGNOUT_SUCCESS':
+            console.log('signout success');
+            return state;
         default:
-        return state;
+            return state;
     }
 }
 
